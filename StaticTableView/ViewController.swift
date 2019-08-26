4ec44b5fd1c896c46b1ce008733718537ca13ec2
MostafaAbdElFatah <m.abdelfatah@gmail.com>
2019-08-26 17:04:04 +0200

    Initial Commit

---
+++ StaticTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StaticTableView
+//
+//  Created by Mostafa Abd ElFatah on 8/26/19.
+//  Copyright © 2019 Mostafa Abd ElFatah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
