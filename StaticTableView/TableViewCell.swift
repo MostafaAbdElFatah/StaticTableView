73d4a06551fb419e7a6418d60f63a277d4b10c7e
MostafaAbdElFatah <m.abdelfatah@gmail.com>
2019-08-26 17:45:50 +0200

    first commit

---
+++ StaticTableView/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  StaticTableView
+//
+//  Created by Mostafa Abd ElFatah on 8/26/19.
+//  Copyright © 2019 Mostafa Abd ElFatah. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
